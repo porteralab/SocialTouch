edbcb1df6d6e2abf1a6575a477168ee520a77a5c
t-chari <45611783+t-chari@users.noreply.github.com>
2025-03-28 19:30:46 -0700

    Add files via upload

---
+++ FormatNPData/GetTrialSVMDataTC090623.m
@@ -0,0 +1,168 @@
+allforcsocialS1KOTRIALS = [];
+
+allforcsocialSTRKOTRIALS = [];
+
+allforcsocialBLAKOTRIALS = [];
+
+allforcobjS1KOTRIALS = [];
+
+allforcobjSTRKOTRIALS = [];
+
+allforcobjBLAKOTRIALS = [];
+
+
+allforcsocialS1WTTRIALS = [];
+
+allforcsocialSTRWTTRIALS = [];
+
+allforcsocialBLAWTTRIALS = [];
+
+allforcobjS1WTTRIALS = [];
+
+allforcobjSTRWTTRIALS = [];
+
+allforcobjBLAWTTRIALS = [];
+
+
+%%
+%% Get trial avgs
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCMOZ;
+
+allforcsocialS1WTTRIALS = [allforcsocialS1WTTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCMOZ;
+
+allforcsocialSTRWTTRIALS = [allforcsocialSTRWTTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCMOZ;
+
+allforcsocialBLAWTTRIALS = [allforcsocialBLAWTTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCOBJZ;
+
+allforcobjS1WTTRIALS = [allforcobjS1WTTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCOBJZ;
+
+allforcobjSTRWTTRIALS = [allforcobjSTRWTTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCOBJZ;
+
+allforcobjBLAWTTRIALS = [allforcobjBLAWTTRIALS; trialdata];
+
+%%
+%%
+%% Get trial avgs 
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCMOZ;
+
+allforcsocialS1KOTRIALS = [allforcsocialS1KOTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCMOZ;
+
+allforcsocialSTRKOTRIALS = [allforcsocialSTRKOTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCMOZ;
+
+allforcsocialBLAKOTRIALS = [allforcsocialBLAKOTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCOBJZ;
+
+allforcobjS1KOTRIALS = [allforcobjS1KOTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCOBJZ;
+
+allforcobjSTRKOTRIALS = [allforcobjSTRKOTRIALS; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCOBJZ;
+
+allforcobjBLAKOTRIALS = [allforcobjBLAKOTRIALS; trialdata];
+
+
+%%
+
+%% Get raw firing for trials
+
+allforcsocialS1KOTRIALSSpks = [];
+
+allforcsocialSTRKOTRIALSSpks = [];
+
+allforcsocialBLAKOTRIALSSpks = [];
+
+allforcobjS1KOTRIALSSpks = [];
+
+allforcobjSTRKOTRIALSSpks = [];
+
+allforcobjBLAKOTRIALSSpks = [];
+
+
+allforcsocialS1WTTRIALSSpks = [];
+
+allforcsocialSTRWTTRIALSSpks = [];
+
+allforcsocialBLAWTTRIALSSpks = [];
+
+allforcobjS1WTTRIALSSpks = [];
+
+allforcobjSTRWTTRIALSSpks = [];
+
+allforcobjBLAWTTRIALSSpks = [];
+
+
+%%
+%% Get trial avgs
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCMOspks;
+
+allforcsocialS1WTTRIALSSpks = [allforcsocialS1WTTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCMOspks;
+
+allforcsocialSTRWTTRIALSSpks = [allforcsocialSTRWTTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCMOspks;
+
+allforcsocialBLAWTTRIALSSpks = [allforcsocialBLAWTTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCOBJspks;
+
+allforcobjS1WTTRIALSSpks = [allforcobjS1WTTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCOBJspks;
+
+allforcobjSTRWTTRIALSSpks = [allforcobjSTRWTTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCOBJspks;
+
+allforcobjBLAWTTRIALSSpks = [allforcobjBLAWTTRIALSSpks; trialdata];
+
+%%
+%%
+%% Get trial avgs 
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCMOspks;
+
+allforcsocialS1KOTRIALSSpks = [allforcsocialS1KOTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCMOspks;
+
+allforcsocialSTRKOTRIALSSpks = [allforcsocialSTRKOTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCMOspks;
+
+allforcsocialBLAKOTRIALSSpks = [allforcsocialBLAKOTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.S1FORCOBJspks;
+
+allforcobjS1KOTRIALSSpks = [allforcobjS1KOTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.STRFORCOBJspks;
+
+allforcobjSTRKOTRIALSSpks = [allforcobjSTRKOTRIALSSpks; trialdata];
+
+trialdata = allFiringProperties.TRIALAVG.BLAFORCOBJspks;
+
+allforcobjBLAKOTRIALSSpks = [allforcobjBLAKOTRIALSSpks; trialdata];
+
+
